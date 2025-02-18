404fdc95d5c89c24a234a55f0ddd974a01c6e179
Igor Djordjevic <igipoison@gmail.com>
2025-02-18 11:21:34 +0100

    auth-and-user-management: Registration of DbContext, Identity and endpoints into the DI container

---
+++ src/TaskManager.API/ProgramExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TaskManager.API.Endpoints;
+using TaskManager.Infrastructure;
+
+namespace TaskManager.API;
+
+public static class ProgramExtensions
+{
+    public static void AppConfigureWebApplication(this WebApplication app)
+    {
+        var configuration = app.Configuration;
+
+        if (configuration.GetValue("UseDeveloperExceptionPage", true))
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapEndpoints();
+    }
+
+    private static WebApplication MapEndpoints(this WebApplication app)
+    {
+        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(app);
+        }
+
+        return app;
+    }
+
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var serviceDescriptors = assembly.GetTypes()
+            .Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+        
+        services.TryAddEnumerable(serviceDescriptors);
+        
+        return services;
+    }
+}
