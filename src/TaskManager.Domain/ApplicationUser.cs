11251f78d478f1aab201b34b3b86ba2e0b7a01d1
Igor Djordjevic <igipoison@gmail.com>
2025-02-18 11:13:08 +0100

    auth-and-user-management: Add ApplicationUser and Tenant models

---
+++ src/TaskManager.Domain/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskManager.Domain;
+
+public class ApplicationUser : IdentityUser
+{
+    public string TenantId { get; set; }
+}
---
+++ src/TaskManager.Domain/Tenant.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Domain;
+
+public class Tenant
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; }
+    public string Domain { get; set; }
+}
