cdd6661e27151d35ba0901953036751424f3b913
Igor Djordjevic <igipoison@gmail.com>
2025-02-18 11:14:51 +0100

    auth-and-user-management: Add DbContext

---
+++ src/TaskManager.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain;
+
+namespace TaskManager.Infrastructure;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<Tenant> Tenants { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Tenant>().HasIndex(t=>t.Domain).IsUnique();
+    }
+}
